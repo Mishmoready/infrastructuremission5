fd9c9d9dda4e6225414a09dd90a64a0140e70343
Mishmoready <moses@missionreadyhq.com>
2023-09-13 08:01:52 +1200

    first committed

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Use an official lightweight Node.js image
+FROM node:18
+
+# Set the working directory in the container
+WORKDIR /mission5/
+
+# Copy package.json and package-lock.json to the working directory
+COPY package*.json ./
+
+# Install application dependencies
+RUN npm install
+
+# Copy the local app files to the container
+COPY . .
+
+# Set the API’s port number
+EXPOSE 3000
+
+# Specify the command to run on container start
+CMD [ "node", "app.js" ]
---
+++ app.js
@@ -0,0 +1,37 @@
+const http = require("http");
+const MongoClient = require("mongodb").MongoClient;
+
+const url = "mongodb://mongo:27017";
+const dbName = "helloWorldDB";
+
+const hostname = "0.0.0.0";
+const port = 3000;
+
+const server = http.createServer(async (req, res) => {
+  let client;
+  try {
+    client = await MongoClient.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    const db = client.db(dbName);
+    const collection = db.collection("messages");
+    await collection.insertOne({ message: "Hello World" });
+
+    const messages = await collection.find({}).toArray();
+
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(messages));
+  } catch (error) {
+    console.error(error);
+    res.statusCode = 500;
+    res.end("Failed to connect to DB");
+  } finally {
+    client.close();
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
